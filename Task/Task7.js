63ddbe410d21c05adc657a6c8cbb2ba3e7b9a3a7
Bright Okiyi <brightokiyi@yahoo.com>
2026-07-13 23:07:55 +0100

    Create Task7.js

---
+++ Task/Task7.js
@@ -0,0 +1,10 @@
+const side1 = 2
+const side2 = 1
+const side3 = 3
+
+if(side1==side2 && side1==side3 && side2==side3){
+    console.log("Equilateral Triangle")
+}else if(side1==side2 || side1==side3 || side2==side3){
+    console.log("Isosceles Triangle")
+}else
+    console.log("Scalene Triangle")
