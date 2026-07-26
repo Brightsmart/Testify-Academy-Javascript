d05c676480cd57b0ce7ae8029f6447641fe7354b
Bright Okiyi <brightokiyi@yahoo.com>
2026-07-26 15:50:42 +0100

    Create Task14.js

---
+++ Task/Task14.js
@@ -0,0 +1,17 @@
+const Books = {
+    title: "Things fall apart",
+    description: "A novel written by Chinua Achebe",
+    numerOfPages: 209,
+    read: true,
+    toggleReadingStatus: function(){
+        if(Books.read === true){
+            Books.read = false
+        }
+        else{
+            Books.read = true
+        }
+    }
+}
+
+//Books.toggleReadingStatus()
+console.log(Books)
