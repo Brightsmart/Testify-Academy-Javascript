e90961ab4e6c95c041c6239b97edbaa08861fb99
Bright Okiyi <brightokiyi@yahoo.com>
2026-07-29 22:12:49 +0100

    more Challenge tasks

---
+++ Challenge/Challenge4.js
@@ -0,0 +1,7 @@
+//const j = 5
+
+for (j = 1; j <= 12; j++){
+
+for (numbers = 1; numbers <= 12; numbers++){
+    console.log(j + " * " + numbers + " = " + (j * numbers))
+}}
---
+++ Challenge/Challenge6.js
@@ -0,0 +1,3 @@
+const toys = ["Teddy Bear", "Big Red Ball", "Huge Bear", "Candy", "Stuffed Tiger", "Stuffed Dragon", "Skateboard", "Toy Car", "Toy Truck", "Rubik's Cube"];
+const alphabeticalorder = toys.sort();
+console.log(alphabeticalorder);  
